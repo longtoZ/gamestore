a9b9d51f33df4e0e39a4255e06929ef841ccad73
Nautilus <longto.xp@gmail.com>
2023-11-24 21:37:48 +0700

    added download link

--- app/products/[productID]/ProductDetail.tsx
+++ app/products/[productID]/ProductDetail.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import {useRouter} from 'next/navigation'
 import Image from 'next/image'
 
+import StarIcon from '@mui/icons-material/Star';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -117,6 +118,103 @@ const ProductDetail: React.FC<ProductDetailProps> = ({ product }) => {
                             <h1 className="my-4 text-xl font-bold">Description</h1>
                             <div className="w-full mb-4" style={{border:'1px solid #a3a3a3'}}></div>
                             <TextWithParagraphs text={product.desc}/>
+                        </div>
+                        <div className="py-6 px-10 my-4 bg-neutral-800 bg-opacity-60 rounded-lg">
+                            <h1 className="my-4 text-xl font-bold">Ratings and reviews</h1>
+                            <div className="w-full mb-4" style={{border:'1px solid #a3a3a3'}}></div>
+                            
+                            <div className="flex">
+                                <div className="my-auto text-center mx-4" style={{width:'20%'}}>
+                                    <h1 className="text-6xl font-bold">4.0</h1>
+                                    <p className="text-neutral-400">1,004 ratings</p>
+                                </div>
+                                <div style={{width:'80%'}}>
+                                    <div className="flex items-center">
+                                        <div className='mx-2' style={{width:'10%'}}>
+                                            <span className='text-neutral-400 px-1'>5</span>
+                                            <StarIcon className='text-yellow-500 text-base'/>
+                                        </div>
+                                        <div className="h-2 w-full rounded-xl bg-yellow-800 relative">
+                                            <div className="h-2 w-2/3 rounded-xl bg-yellow-500 absolute top-0 left-0 z-10"></div>
+
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className='mx-2' style={{width:'10%'}}>
+                                            <span className='text-neutral-400 px-1'>4</span>
+                                            <StarIcon className='text-yellow-500 text-base'/>
+                                        </div>
+                                        <div className="h-2 w-full rounded-xl bg-yellow-800 relative">
+                                            <div className="h-2 w-1/2 rounded-xl bg-yellow-500 absolute top-0 left-0 z-10"></div>
+
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className='mx-2' style={{width:'10%'}}>
+                                            <span className='text-neutral-400 px-1'>3</span>
+                                            <StarIcon className='text-yellow-500 text-base'/>
+                                        </div>
+                                        <div className="h-2 w-full rounded-xl bg-yellow-800 relative">
+                                            <div className="h-2 w-1/3 rounded-xl bg-yellow-500 absolute top-0 left-0 z-10"></div>
+
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className='mx-2' style={{width:'10%'}}>
+                                            <span className='text-neutral-400 px-1'>2</span>
+                                            <StarIcon className='text-yellow-500 text-base'/>
+                                        </div>
+                                        <div className="h-2 w-full rounded-xl bg-yellow-800 relative">
+                                            <div className="h-2 w-1/4 rounded-xl bg-yellow-500 absolute top-0 left-0 z-10"></div>
+
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <div className='mx-2' style={{width:'10%'}}>
+                                            <span className='text-neutral-400 px-1'>1</span>
+                                            <StarIcon className='text-yellow-500 text-base'/>
+                                        </div>
+                                        <div className="h-2 w-full rounded-xl bg-yellow-800 relative">
+                                            <div className="h-2 w-1/4 rounded-xl bg-yellow-500 absolute top-0 left-0 z-10"></div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* <div className=" mt-8 w-full mb-4" style={{borderBottom:'1px solid #a3a3a3'}}></div> */}
+
+                            <div>
+                                <div className="flex mt-14 px-2">
+                                    <div style={{width:'90%'}}>
+                                        <input className='w-full rounded-full bg-black bg-opacity-20 p-2' type="text" placeholder="Write your thoughts..." />
+                                    </div>
+                                    <div className='px-2' style={{width:'10%'}}>
+                                        <button className='w-full px-3 py-2 rounded-full bg-gradient-to-br from-green-400 to-blue-500 text-white'>Post</button>
+                                    </div>
+                                </div>
+
+
+                                <div className="mt-10 px-4">
+                                    <div className="mb-10">
+                                        <ul className="list-disc">
+                                            <li className="text-neutral-400">LongTo <span className="px-4 text-neutral-500 text-sm">1 month ago</span></li>
+                                        </ul>
+                                        <h1 className="text-xl font-bold">Very attractive gameplay</h1>
+                                        <Rating className='text-sm mb-4' value={product.rating} readOnly/>
+                                        <p className="">I have been playing this game for a month but it is still so interesting. I wanna share this game to my friends!!!</p>
+                                    </div>
+
+                                    <div className="mb-10">
+                                        <ul className="list-disc">
+                                            <li className="text-neutral-400">RandomUser123  <span className="px-4 text-neutral-500 text-sm">2 weeks ago</span></li>
+                                        </ul>
+                                        <h1 className="text-xl font-bold">Cant stop playing this game</h1>
+                                        <Rating className='text-sm mb-4' value={product.rating} readOnly/>
+                                        <p className="">I spend 2 hours per day just to discover new things in the game. I hope the developer will make more stuff for us to try out!</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
